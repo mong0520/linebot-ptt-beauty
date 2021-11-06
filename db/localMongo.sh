a916057d87531c298e0b084c9484c3646897cd59
Evan Lin <evan.if.lin@linecorp.com>
2021-11-06 21:30:06 +0800

    install go-pg and related init code.

--- db/localMongo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo $(PWD)/volume
-mkdir -p $(PWD)/volume
-docker run -p 27017:27017 -v $(PWD)/volume:/data/db mongo
--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"os"
 	"path"
 
+	"github.com/go-pg/pg/v10"
+	"github.com/go-pg/pg/v10/orm"
 	"github.com/kkdai/linebot-ptt-beauty/bots"
+	"github.com/kkdai/linebot-ptt-beauty/controllers"
 	"github.com/kkdai/linebot-ptt-beauty/models"
 	"github.com/kkdai/linebot-ptt-beauty/utils"
 )
@@ -18,6 +21,16 @@ func main() {
 	logFile, err := initLogFile()
 	// dbHostPort := os.Getenv("MongoDBHostPort")
 	defer logFile.Close()
+
+	options, _ := pg.ParseURL(os.Getenv("DATABASE_URL"))
+	db := pg.Connect(options)
+	meta.Db = db
+	defer db.Close()
+
+	err = createSchema(db)
+	if err != nil {
+		panic(err)
+	}
 
 	if err != nil {
 		logger.Fatalln("open file error !")
@@ -38,4 +51,20 @@ func initLogFile() (logFile *os.File, err error) {
 		os.Mkdir(logRoot, 0755)
 	}
 	return os.OpenFile(logFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+}
+
+func createSchema(db *pg.DB) error {
+	models := []interface{}{
+		(*controllers.UserFavorite)(nil),
+	}
+
+	for _, model := range models {
+		err := db.Model(model).CreateTable(&orm.CreateTableOptions{
+			IfNotExists: true,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
